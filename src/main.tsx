1561cc63e2ffad303f61eb3ae06b098a71756e04
MarvinPateau <veromar046@gmail.com>
2025-08-08 11:24:17 +0200

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />
+  </React.StrictMode>
+);
